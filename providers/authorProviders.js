b9bf6b52c2b4d6fa4fefe4208c4616e4e774572c
albeada <albertoamato207@gmail.com>
2025-12-15 18:54:16 +0100

    gira

---
+++ providers/authorProviders.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+
+function getAllUsers () {
+    return JSON.parse(fs.readFileSync('./providers/users.json', {
+        encoding: 'utf8'
+    }))
+}
+
+function getUser(username){
+    const users= getAllUsers();
+    return users.find(users => users.username === username);
+}
+
+function getUserbyId(id){
+    const users= getAllUsers();
+    return users.find(users => users.id === id);
+}
+
+
+function removeUser(username){
+    const users= getAllUsers();
+    let user2 = users.filter(users => users.username !== username);
+    saveUser(user2);
+}
+
+
+function updateUser(id, username, name, surname, age){
+    //const {username, name, surname, age} = user;
+    let user = getUserbyId(id);
+
+    user = {
+        "id": parseInt(id, 10),
+        "username": username ? username : user.username,
+        "name": name ? name : user.name,
+        "surname": surname ? surname : user.surname,
+        "age": age ? age : user.age
+    }
+
+    saveUser(user);
+    return user;
+}
+
+function saveUser(users){
+    fs.writeFileSync('users.json', JSON.stringify(users, null, 2), 'utf8');
+}
+
+function addUser(username, name, surname, age){
+    const users = getAllUsers();
+    if(getUser(username) == undefined){
+        let user = {"username": username, "name": name, "surname": surname, "age": parseInt(age)};
+        users.push(user);
+        saveUser(users);
+    }
+    return users;    
+}
+
+module.exports={
+    getAllUsers,
+    getUser,
+    getUserbyId,
+    removeUser,
+    updateUser,
+    addUser
+}
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const authorsProvider = require("./providers/authorProviders.js")
+
+app.use(express.json());
+
+app.get('/authors', (req, res) => {
+    res.json(authorsProvider.getAllUsers());
+});
+
+app.get("/authors/:username", (req, res) => {
+    const { username } = req.params;
+    console.log(username);
+    res.json(authorsProvider.getUser(username));
+});
+
+/* prova
+app.get("/authors/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    res.json(authorsProvider.getUserbyId(parseInt(id, 10)));
+});
+*/
+
+app.get("/authors", (req, res) => {
+   const { age } = req.query;//querry usato per i filtri
+   console.log(age);
+   res.json(authorsProvider.getAllUsers());
+});
+
+//patch usate per la modifica di un autore specifico
+app.patch("/authors/:id", (req, res) => {
+   const { id } = req.params;
+   const{ username, name, surname, age } = req.body;
+
+    res.json(authorsProvider.updateUser(id, username, name, surname, age));
+});
+
+
+//delete usato per cancelare un autore
+app.delete("/authors/:username", (req, res) => {
+   const { username } = req.params;
+   res.json(authorsProvider.removeUser(username));
+});
+
+app.put("/authors", (req, res) => {
+   const{ id,username, name, surname, age } = req.body;
+   res.json(authorsProvider.addUser(id, username, name, surname, age ));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listen on port %{port}`);
+});
